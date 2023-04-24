ddb215e4e8254226553ff2b9c59cb58a5411edf7
Davide Tarpini <davidetarpini@gmail.com>
2023-04-24 21:12:33 +0200

    Fixed typo and scripts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/assets/css");
     eleventyConfig.addPassthroughCopy("src/assets/js");
